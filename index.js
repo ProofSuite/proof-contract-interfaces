c3b93c4f97088b552f057cb19d9562522c7644ed
David Van Isacker <davidvanisacker@gmail.com>
2018-05-12 02:03:32 +0900

    [#157517433] Add dex and cryptodollar-b contract interfaces

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 module.exports = {
-	CryptoDollar: require('./CryptoDollar.json'),
-	CryptoFiatHub: require('./CryptoFiatHub.json'),
-	Rewards: require('./Rewards.json'),
-	Store: require('./Store.json')
+  cryptoDollarA: {
+    CryptoDollar: require('./cryptodollar-a/CryptoDollar.json'),
+    CryptoFiatHub: require('./cryptodollar-a/CryptoFiatHub.json'),
+    Rewards: require('./cryptodollar-a/Rewards.json'),
+    Store: require('./cryptodollar-a/Store.json')
+  },
+  cryptoDollarB: {
+    CryptoDollar: require('./cryptodollar-b/CryptoDollar.json'),
+    CryptoFiatHub: require('./cryptodollar-b/CryptoFiatHub.json'),
+    Rewards: require('./cryptodollar-b/Rewards.json'),
+    Store: require('./cryptodollar-b/Store.json')
+  },
+  dex: {
+    Exchange: require('./dex/Exchange.json'),
+    Token1: require('./dex/Token1.json'),
+    Token2: require('./dex/Token2.json')
+  }
 }

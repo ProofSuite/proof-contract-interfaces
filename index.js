bdb0470ad08556adc6135abaf0d94a747645f6e4
David Van Isacker <davidvanisacker@gmail.com>
2018-06-17 17:26:41 +0900

    Updated dex contracts

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ module.exports = {
   CryptoFiatHub: require('./cryptodollar-a/CryptoFiatHub.json'),
   Rewards: require('./cryptodollar-a/Rewards.json'),
   Store: require('./cryptodollar-a/Store.json'),
+  Exchange: require('./dex/Exchange.json'),
+  ERC20Token: require('./dex/Token1.json'),
+  Token1: require('./dex/Token1.json'),
+  Token2: require('./dex/Token2.json'),
+  Token3: require('./dex/Token3.json'),
   CryptoDollarBis: {
     CryptoDollar: require('./cryptodollar-b/CryptoDollar.json'),
     CryptoFiatHub: require('./cryptodollar-b/CryptoFiatHub.json'),
     Rewards: require('./cryptodollar-b/Rewards.json'),
     Store: require('./cryptodollar-b/Store.json')
-  },
-  dex: {
-    Exchange: require('./dex/Exchange.json'),
-    Token1: require('./dex/Token1.json'),
-    Token2: require('./dex/Token2.json')
   }
 }
